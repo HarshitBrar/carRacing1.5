0f3fa51858f96a7793f24eaae48a09d6fc675b1e
HarshitBrar <harshitbrar.1305@gmail.com>
2021-01-19 19:41:28 +0530

    hello

--- sketch.js
+++ sketch.js
@@ -9,8 +9,15 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
-
-
+var image1,image2,image3,image4;
+var track;
+function preload(){
+  image1 = loadImage("images/car1.png");
+  image2 = loadImage("images/car2.png");
+  image3 = loadImage("images/car3.png");
+  image4 = loadImage("images/car4.png");
+  track = loadImage("images/track.jpg");
+}
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
   database = firebase.database();
@@ -27,5 +34,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState === 2){
+    game.end();
   }
 }
